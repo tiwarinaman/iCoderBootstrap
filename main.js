41b619226792841954b12de3acde75bb564a18fb
Naman Kumar <49829185+tiwarinaman@users.noreply.github.com>
2021-09-11 23:56:22 +0530

    made new function for api hit

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+function hitApi() {
+ var xhr = new XMLHttpRequest();
 
+xhr.onload = function () {
+
+	if (xhr.status >= 200 && xhr.status < 300) {
+    
+    console.log("Success Data", JSON.parse(xhr.response);)
+	} else {
+      console.log('The request failed!');
+	}
+
+	console.log('This always runs...');
+};
+
+xhr.open('GET', 'https://gorest.co.in/public/v1/users');
+xhr.send(); 
+}
